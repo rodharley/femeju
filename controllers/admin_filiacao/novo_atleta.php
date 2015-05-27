a559a4ab78140f39ddd15c52fd48e6722312b6ea
Rodrigo Cruz <rodrigo.cruz76@gmail.com>
2015-05-27 14:26:56 -0300

    filiacao novo atleta

--- controllers/admin_filiacao/novo_atleta.php
+++ controllers/admin_filiacao/novo_atleta.php
@@ -10,18 +10,18 @@ $usu = new Noticia();
 //CONFIGURA O BREADCRUMB
 $TPL->BREADCRUMB = '<section class="content-header">
 			                        <h1>
-			                            Notícias
-			                            <small>Pesquisa</small>
+			                            Filiação
+			                            <small>Cadastro de novo Atleta</small>
 			                        </h1>
 			                        <ol class="breadcrumb">
-			                            <li><a href="admin_home-home"><i class="fa fa-dashboard"> </i> Home</a></li>
-			                            <li><a href="admin_noticia-main"><i class="fa fa-newspaper-o"> </i> Notícias</a></li>
-			                            <li class="active">Pesquisa</li>
+			                        <li><a href="admin_home-home"><i class="fa fa-dashboard"> </i> Home</a></li>
+                                        <li><a href="admin_noticia-main"><i class="fa fa-pencil"> </i> Filiação</a></li>
+			                            <li class="active">Novo Atleta</li>
 			                        </ol>
                 </section>';
 
 //TRATA O CONTEUDO------------------------------------------------------------------------------------------------------------
-$TPL->addFile("CONTEUDO", "templates/admin/noticia/pesquisa.html");
+$TPL->addFile("CONTEUDO", "templates/admin/filiacao/novo.html");
 
 
 
--- js/plugins/datepicker/locales/bootstrap-datepicker.pt-BR.js
+++ js/plugins/datepicker/locales/bootstrap-datepicker.pt-BR.js
@@ -4,10 +4,10 @@
  */
 ;(function($){
 	$.fn.datepicker.dates['pt-BR'] = {
-		days: ["Domingo", "Segunda", "TerÃ§a", "Quarta", "Quinta", "Sexta", "SÃ¡bado", "Domingo"],
-		daysShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "SÃ¡b", "Dom"],
+		days: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo"],
+		daysShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb", "Dom"],
 		daysMin: ["Do", "Se", "Te", "Qu", "Qu", "Se", "Sa", "Do"],
-		months: ["Janeiro", "Fevereiro", "MarÃ§o", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+		months: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
 		monthsShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
 		today: "Hoje",
 		clear: "Limpar"
