c8872cd6dc9b780d3e121a780c6708abb2cb8cfc
Rodrigo Cruz <rodrigo.cruz76@gmail.com>
2015-08-05 08:01:14 -0300

    Paginas

--- class_modelo/negocio.pagina.php
+++ class_modelo/negocio.pagina.php
@@ -1,7 +1,10 @@
 <?php
 class Pagina extends Persistencia{
 	const PRESIDENCIA = 1;
-	const DIRETORIA = 2;		
+	const DIRETORIA = 2;
+    const LINKS = 3;
+    const AFEMEJU = 4;
+    const CONTATO = 5;		
 	var $conteudo;
     var $nome;
     var $titulo;
