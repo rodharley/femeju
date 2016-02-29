5c51d97ad233240e6ce0a68bc5060b3793df34d7
Rodrigo Cruz <rodrigo.cruz76@gmail.com>
2016-02-29 14:04:09 -0300

    RSS

--- controllers/rss/main.php
+++ controllers/rss/main.php
@@ -6,9 +6,9 @@ $canal = $rss->addChild('channel');
 $now = time();
 $pubDate= date('r', $now);
 // Adiciona sub-elementos ao elemento <channel>
-$canal->addChild('title', 'Notícias Judo Brasília');
+$canal->addChild('title', utf8_encode('Notícias Judo Brasília'));
 $canal->addChild('link', 'http://www.judobrasilia.com.br');
-$canal->addChild('description', 'Rss de notícias do site judo brasília');
+$canal->addChild('description', utf8_encode('Rss de notícias do site judo brasília'));
 $canal->addChild('pubDate', $pubDate);
 
 $objNoticia = new Noticia();
@@ -17,11 +17,11 @@ foreach ($rs as $key => $not) {
     // Cria um elemento <item> dentro de <channel>
 $item = $canal->addChild('item');
 // Adiciona sub-elementos ao elemento <item>
-$item->addChild('title', $not->titulo);
+$item->addChild('title', utf8_encode($not->titulo));
 $item->addChild('link', URL.'/portal_noticia-detalhe?id='.$objNoticia->md5_encrypt($not->id));
-$item->addChild('description', $not->sumario);
+$item->addChild('description', utf8_encode($not->sumario));
 $dt = strtotime($not->data);
-$canal->addChild('pubDate', $dt);
+$item->addChild('pubDate', date('r',$dt));
 
 	
 }
@@ -30,7 +30,7 @@ $canal->addChild('pubDate', $dt);
 
 // Define o tipo de conteúdo e o charset
 //header("content-type: application/rss+xml; charset=utf-8");
-header('Content-Type: text/xml');
+//header('Content-Type: text/xml');
 //header('Content-Type: application/xml');
 // Entrega o conteúdo do RSS completo:
 echo $rss->asXML();
