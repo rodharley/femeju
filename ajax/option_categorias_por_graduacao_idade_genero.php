acb9483802d0b18a94dd0243e90a952c2e259d2f
Rodrigo Cruz <rodrigo.cruz76@gmail.com>
2018-03-19 21:28:11 -0300

    correçao na inscricao aberta idcompeticao na busca das classses

--- ajax/option_categorias_por_graduacao_idade_genero.php
+++ ajax/option_categorias_por_graduacao_idade_genero.php
@@ -3,22 +3,27 @@ $menu = 0;
 include ("configuraAjax.php");
 $obj = new ClasseGraduacao();
 $objCat = new CategoriaPeso();
+$objGrupoCompeticao = new GrupoCompeticao();
 $idade = $_REQUEST['idade'];
 $genero = $_REQUEST['genero'];
+$idCompeticao = isset($_REQUEST['idcompeticao']) ? $_REQUEST['idcompeticao'] : 0;
 $lista = $obj -> getRows(0, 99, array(), array("graduacao" => "=" . $_REQUEST['graduacao']));
 echo "<option value=''>selecione</option>";
 foreach ($lista as $key => $value) {
 	if ($idade <= $value -> classe -> maximo && $idade >= $value -> classe -> minimo) {
+		if ($objGrupoCompeticao -> getRow(array("classe" => "=" . $value -> classe -> id, "competicao" => "=" . $idCompeticao))) {
+		
 		echo "<optgroup label='" . $value -> classe -> descricao . "'>";
 		if ($genero != "") {
-			$lista = $objCat -> listaAtivasPorClasseGenero($value -> classe->id, $genero);
+			$lista2 = $objCat -> listaAtivasPorClasseGenero($value -> classe->id, $genero);
 		} else {
-			$lista = $objCat -> listaAtivasPorClasse($value -> classe->id);
+			$lista2 = $objCat -> listaAtivasPorClasse($value -> classe->id);
 		}
-		foreach ($lista as $key2 => $value2) {
+		foreach ($lista2 as $key2 => $value2) {
 			echo "<option value='" . $value2 -> id . "'>" . $value2 -> descricao." Peso:".$value2 -> minimo."Kg até ".$value2 -> maximo . "Kg" . "</option>";
 		}
 		echo "</optgroup>";
+	}
 	}
 }
 exit();
--- ajax/option_classes_por_graduacao_idade.php
+++ ajax/option_classes_por_graduacao_idade.php
@@ -2,12 +2,16 @@
 $menu = 0;
 include("configuraAjax.php");
 $obj = new ClasseGraduacao();
+$objGrupoCompeticao = new GrupoCompeticao();
 $idade = $_REQUEST['idade'];
+$idCompeticao = isset($_REQUEST['idcompeticao']) ? $_REQUEST['idcompeticao'] : 0;
 $lista = $obj->getRows(0,99,array(),array("graduacao"=>"=".$_REQUEST['graduacao']));
 echo "<option value=''>selecione</option>";
 foreach ($lista as $key => $value) {
-	if ($idade <= $value -> classe -> maximo && $idade >= $value -> classe -> minimo) {    
-    echo "<option value='".$value->classe->id."'>".$value->classe->descricao."</option>";
+	if ($idade <= $value -> classe -> maximo && $idade >= $value -> classe -> minimo) {
+	if ($objGrupoCompeticao -> getRow(array("classe" => "=" . $value -> classe -> id, "competicao" => "=" . $idCompeticao))) {
+	    echo "<option value='".$value->classe->id."'>".$value->classe->descricao."</option>";
+	}
 	}
 }
 exit();
