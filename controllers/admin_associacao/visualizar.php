e952e0e012c14f3fe0e3baeeecf74960c155adb7
Rodrigo Cruz <rodrigo.cruz76@gmail.com>
2018-07-20 15:05:16 -0300

    visualizar associacoes

---
+++ controllers/admin_associacao/visualizar.php
@@ -0,0 +1,134 @@
+<?php
+$menu = 25;
+include("includes/include.lock.php");
+$TPL = NEW Template("templates/admin/main.html");
+include("includes/include.montaMenu.php");
+include("includes/include.mensagem.php");
+
+$objAssociacao = new Associacao();
+$uf = new Uf();
+$cidade = new Cidade();
+$objUsuario = new Usuario(); 
+//CONFIGURA O BREADCRUMB
+$TPL->BREADCRUMB = '<section class="content-header">
+                    <h1>
+                        Associação
+                        <small>Edição de Associação</small>
+                    </h1>
+                   <ol class="breadcrumb">
+                                        <li><a href="admin_home-home"><i class="fa fa-home"> </i> Home</a></li>
+                                        <li><a href="admin_associacao-main"><i class="fa fa-users"> </i> Associações</a></li>
+                                         <li class="active">Visualizar</li>
+                                    </ol>
+                </section>';
+
+//TRATA O CONTEUDO------------------------------------------------------------------------------------------------------------
+$TPL->addFile("CONTEUDO", "templates/admin/associacao/edit.html");
+
+$TPL->disabled ="disabled";
+$TPL->LABEL = "Associação";
+$TPL->ACAO = "visualizar";
+$TPL->id = 0;
+$TPL->LOGOMARCA = "";
+$selectedUf = 0;
+$selectedCidade = 0;
+$selectedUf_resp = 0;
+$selectedCidade_resp = 0;
+$selected_resp = 0;
+$TPL->checkedAtivo = "checked";
+$TPL->checkedInativo = "";
+$listaUf = $uf->getRows();
+
+$listaResp =  $objUsuario->listarUsuariosResponsavel();
+
+if(isset($_REQUEST['id'])){
+	$objAssociacao->getById($objAssociacao->md5_decrypt($_REQUEST['id']));
+	$TPL->nome = $objAssociacao->nome;
+    $TPL->razaoSocial = $objAssociacao->razaoSocial;
+	$TPL->descricao = $objAssociacao->descricao;
+    $TPL->sigla = $objAssociacao->sigla;
+    $TPL->cnpj = $objAssociacao->cnpj;
+    $TPL->identificacao = $objAssociacao->identificacao;
+    $selected_resp = $objAssociacao->responsavel->id;
+    
+    if($objAssociacao->dataFiliacao != null)
+        $TPL->dataFiliacao = $objAssociacao->convdata($objAssociacao->dataFiliacao,"mtn");
+	$TPL->endereco = $objAssociacao->endereco;
+    $TPL->bairro = $objAssociacao->bairro;
+    if($objAssociacao->cidade != null){
+         $selectedUf   = $objAssociacao->cidade->uf->id;
+        $selectedCidade = $objAssociacao->cidade->id;
+        $listaCidade = $cidade->getRows(0,9999,array("nome"=>"ASC"),array("uf"=>"=".$objAssociacao->cidade->uf->id));
+        foreach ($listaCidade as $key => $value) {
+                 $TPL->selectedCidade = "";
+                  $TPL->nome_cidade = $value->nome;
+                  $TPL->id_cidade = $value->id;
+                  if($selectedCidade == $value->id)
+                    $TPL->selectedCidade = "selected";
+                  $TPL->block("BLOCK_CIDADE");
+              }     
+    }
+    
+    if($objAssociacao->responsavel->pessoa->cidade != null){
+         $selectedUf_resp   = $objAssociacao->responsavel->pessoa->cidade->uf->id;
+        $selectedCidade_resp = $objAssociacao->responsavel->pessoa->cidade->id;
+        $listaCidade = $cidade->getRows(0,9999,array("nome"=>"ASC"),array("uf"=>"=".$objAssociacao->responsavel->pessoa->cidade->uf->id));
+        foreach ($listaCidade as $key => $value) {
+                 $TPL->selectedCidaderesp = "";
+                  $TPL->nome_cidade_resp = $value->nome;
+                  $TPL->id_cidade_resp = $value->id;
+                  if($selectedCidade_resp == $value->id)
+                    $TPL->selectedCidaderesp = "selected";
+                  $TPL->block("BLOCK_CIDADE_RESP");
+              }     
+    }
+    
+    $TPL->cep = $objAssociacao->cep;    
+    $TPL->telefone1 = $objAssociacao->telefone1;
+    $TPL->telefone2 = $objAssociacao->telefone2;
+    $TPL->email = $objAssociacao->email;
+    $TPL->website = $objAssociacao->webSite;
+    $TPL->midiaSocial = $objAssociacao->midiaSocial;
+	$TPL->id = $objAssociacao->id;
+       
+    
+	$TPL->LOGOMARCA = "img/associacoes/".$objAssociacao->logomarca;
+	$TPL->LABEL = "Alterar Associação ".$objAssociacao->nome;
+	$TPL->ACAO = "editar";
+	if(strlen($objAssociacao->logomarca) > 0){
+		$TPL->LOGOMARCA = "<img src='img/associacoes/".$objAssociacao->logomarca."' class='file-preview-image' alt='".$objAssociacao->logomarca."' title='".$objAssociacao->logomarca."'>";
+		$TPL->block("BLOCK_IMG");
+	}
+    
+    foreach ($objAssociacao->fotos as $key => $foto) {
+        $TPL->FOTOS = "<img src='img/associacoes/".$foto->imagem."' class='file-preview-image' alt='".$foto->imagem."' title='".$foto->imagem."'><br/><button value='".$foto->id."' class='btn btn-default btExcluirImagem' type='button'><i class='glyphicon glyphicon-ban-circle'></i> Apagar</button>";
+        $TPL->block("BLOCK_FOTOS");
+    }   
+	
+}
+
+ foreach ($listaUf as $key => $value) {
+     $TPL->selectedUf = "";
+     $TPL->selectedUfresp = "";
+      $TPL->uf = $value->uf;
+      $TPL->id_uf = $value->id;
+      if($selectedUf == $value->id)
+        $TPL->selectedUf = "selected";
+      if($selectedUf_resp == $value->id)
+        $TPL->selectedUfresp = "selected";
+      $TPL->block("BLOCK_UF");
+      $TPL->block("BLOCK_UF_RESP");
+  }
+  
+   foreach ($listaResp as $key => $value) {
+      $TPL->ID_RESP = $value->id;
+      $TPL->LABEL_RESP = $objUsuario->formataCPFCNPJ($value->pessoa->cpf)."-".$value->pessoa->getNomeCompleto();
+      $TPL->CHECKED_RESP = "";
+      if($selected_resp == $value->id)
+        $TPL->CHECKED_RESP = "selected";
+      
+      $TPL->block("BLOCK_RESPONSAVEL");
+      
+  }     
+$TPL->show();
+?>
